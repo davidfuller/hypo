08947ca9880635bcd543f8d7d86cdf502c1b8936
davidfuller <david@muvi2.co.uk>
2020-06-25 20:22:51 +0100

    machine socket commands

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,2 +1,26 @@
 class Machine < ApplicationRecord
+
+  require 'socket'
+
+  def self.connect
+    "Hello"
+  end
+
+  def self.play
+    socket = TCPSocket.open("192.168.1.168", 9993)
+    socket.puts "play\n"
+    a = []
+    while message = socket.gets             # Read lines from the socket
+      a.push(message.chomp)
+    end
+    socket.close
+    a
+  end
+
+  def self.stop
+    socket = TCPSocket.open("192.168.1.168", 9993)
+    socket.puts "stop\n"
+    socket.close
+    "Done"
+  end
 end
