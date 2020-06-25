58364ee0145bf343eb62e04a407852a031a4cf86
David Fuller <davidfuller@users.noreply.github.com>
2020-06-25 21:10:29 +0100

    A good tidy up

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -3,29 +3,29 @@ class Machine < ApplicationRecord
   require 'socket'
 
   def self.connect
+    @socket = TCPSocket.open("192.168.1.168", 9993)
     "Hello"
   end
-
+  
+  def self.close
+    @socket.close
+    "Closed"
+  end
+  
   def self.play
-    socket = TCPSocket.open("192.168.1.168", 9993)
-    socket.puts "play\n"
-    messages = read_message(socket)
-    socket.close
-    messages
+    @socket.puts "play\n"
+    read_message
   end
 
   def self.stop
-    socket = TCPSocket.open("192.168.1.168", 9993)
-    socket.puts "stop\r\n"
-    messages = read_message(socket)
-    socket.close
-    messages
+    @socket.puts "stop\r\n"
+    read_message
   end
   
   private
-  def self.read_message(sock)
+  def self.read_message
     lines = []
-    while (line = sock.gets) && line.chomp != '' # While the client is connected, and hasn't sent us a blank line yet...
+    while (line = @socket.gets) && line.chomp != '' # While the client is connected, and hasn't sent us a blank line yet...
       lines << line.chomp
     end
     lines
